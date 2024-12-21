2755fc8634202aac07c83f1e3f191f35508e56ac
Akash-ojha-AJ2 <jhak2594@gmail.com>
2024-12-21 12:25:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(session({
 
 // CORS configuration
 const corsOptions = {
-  origin: "http://localhost:5173", // Frontend URL
+  origin: 'https://frontend1-pq6z.onrender.com', // Frontend URL
   methods: "GET, POST, PUT, DELETE, PATCH",
   credentials: true,
 };
