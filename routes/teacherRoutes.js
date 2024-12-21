3f5a1e9ad494a4bc771f5a3891e40e9c4d92dc7d
Akash-ojha-AJ2 <jhak2594@gmail.com>
2024-12-21 21:48:43 +0530

    Update teacherRoutes.js

--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -4,6 +4,7 @@ const routess = require('../controllers/teacher-controller');
 const student = require('../controllers/student-controller');
 const validate = require("../middleware/validate-middleware");
 const registerSchema = require("../validators/teacherValidator");
+const authMiddleware = require('../middleware/authenticateTeacher.js.js');
 
 
 
@@ -17,7 +18,7 @@ router
     .post(validate(registerSchema), routess.register);
 
 router.route('/login').post(routess.login);
-router.route('/logout').post(routess.logout);
+router.route('/logout').post(authMiddleware,routess.logout);
 router.route('/profile').get(routess.profile);
 
 module.exports = router;
