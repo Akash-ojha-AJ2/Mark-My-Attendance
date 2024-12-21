e19cfa7983acb95f4f3d337b1b3a0aff1ade7f03
Akash-ojha-AJ2 <jhak2594@gmail.com>
2024-12-21 14:46:19 +0530

    Update studentRoutes.js

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -7,15 +7,15 @@ const authMiddleware = require('../middleware/authenticateTeacher.js.js');
 
 // Routes for adding and managing students
 router.route("/addstudent").post(authMiddleware,student.addStudent);
-router.route("/viewstudentdataform").post(student.viewstudentdataform);
-router.route("/attendancedataform").post(student.attendancedataform);
-router.route('/attendancedataform/:branch/:semester').get(student.attendancedataform);
-router.route('/:branch/:semester').get(student.deletestudentform);
-router.route('/delete/:studentId').delete(student.deleteStudent);
-router.route('/deleteall/:branch/:semester').delete(student.deleteAllStudents);
+router.route("/viewstudentdataform").post(authMiddleware,student.viewstudentdataform);
+router.route("/attendancedataform").post(authMiddleware,student.attendancedataform);
+router.route('/attendancedataform/:branch/:semester').get(authMiddleware,student.attendancedataform);
+router.route('/:branch/:semester').get(authMiddleware,student.deletestudentform);
+router.route('/delete/:studentId').delete(authMiddleware,student.deleteStudent);
+router.route('/deleteall/:branch/:semester').delete(authMiddleware,student.deleteAllStudents);
 
 // Route for marking attendance
-router.route('/attendance').post(student.markAttendance);
+router.route('/attendance').post(authMiddleware,student.markAttendance);
 
 module.exports = router;
 
